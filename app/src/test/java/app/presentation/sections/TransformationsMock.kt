031f198196f0b88e8e29809c416fc7a463992b29
Victor Albertos <me@victoralbertos.com>
2017-03-14 00:11:06 +0100

    convert UserRepository

---
+++ app/src/test/java/app/presentation/sections/TransformationsMock.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Victor Albertos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.presentation.sections
+
+import app.presentation.foundation.transformations.Transformations
+import io.reactivex.Single
+import io.reactivex.SingleTransformer
+
+class TransformationsMock : Transformations {
+    override fun setLifecycle(lifecycle: SingleTransformer<*, *>) {
+        // Do nothing.  Exists to satisfy Transformations.
+    }
+
+    override fun <T : Any> safely(): SingleTransformer<T, T> = SingleTransformer { it }
+
+    override fun <T : Any> reportOnSnackBar(): SingleTransformer<T, T> = SingleTransformer {
+        it.onErrorResumeNext {
+            Single.never()
+        }
+    }
+
+    override fun <T : Any> reportOnToast(): SingleTransformer<T, T> = SingleTransformer {
+        it.onErrorResumeNext {
+            Single.never()
+        }
+    }
+
+    override fun <T : Any> loading(): SingleTransformer<T, T> = SingleTransformer { it }
+
+    override fun <T : Any> loading(content: String?): SingleTransformer<T, T> = SingleTransformer { it }
+}
