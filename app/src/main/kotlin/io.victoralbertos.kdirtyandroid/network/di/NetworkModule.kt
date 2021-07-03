d9c27c2d158c7c7bc9e38bee2c1c5ad0865cc6fb
Victor Albertos Gil <me@victoralbertos.com>
2021-07-03 18:37:18 +0200

    bump several dependencies version

--- app/src/main/kotlin/io.victoralbertos.kdirtyandroid/network/di/NetworkModule.kt
+++ app/src/main/kotlin/io.victoralbertos.kdirtyandroid/network/di/NetworkModule.kt
@@ -4,13 +4,13 @@ import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import io.victoralbertos.kdirtyandroid.network.api.GithubUsersApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     private const val URL_BASE_GITHUB = "https://api.github.com"
 
--- app/src/main/kotlin/io.victoralbertos.kdirtyandroid/presentation/home/users/UsersViewModel.kt
+++ app/src/main/kotlin/io.victoralbertos.kdirtyandroid/presentation/home/users/UsersViewModel.kt
@@ -1,18 +1,20 @@
 package io.victoralbertos.kdirtyandroid.presentation.home.users
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.victoralbertos.kdirtyandroid.entities.User
 import io.victoralbertos.kdirtyandroid.paging.UsersPagingSource
 import io.victoralbertos.kdirtyandroid.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class UsersViewModel @ViewModelInject constructor(
+@HiltViewModel
+class UsersViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
     val pagingData: Flow<PagingData<User>> = Pager(PagingConfig(pageSize = 20)) { UsersPagingSource(userRepository) }
